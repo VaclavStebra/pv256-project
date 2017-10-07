4329e0c6c88986a8b8fa6fae40ffff4886d760e5
Vaclav Stebra <vstebra@gmail.com>
2017-10-07 15:10:04 +0200

    Main activity with button and recycler view
    Toggle theme when button is clicked.

--- app/src/main/java/cz/muni/fi/pv256/movio2/uco_422186/MainActivity.java
+++ app/src/main/java/cz/muni/fi/pv256/movio2/uco_422186/MainActivity.java
@@ -1,13 +1,59 @@
 package cz.muni.fi.pv256.movio2.uco_422186;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setTheme(getSavedTheme());
         setContentView(R.layout.activity_main);
+
+        Button changeThemeBtn = (Button) findViewById(R.id.change_theme_button);
+
+        changeThemeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toggleTheme();
+            }
+        });
+    }
+
+    private int getSavedTheme() {
+        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+        return sharedPref.getInt(getString(R.string.saved_theme), R.style.PrimaryTheme);
+    }
+
+    private void toggleTheme() {
+        int theme = getSavedTheme();
+        saveNewTheme(getNewTheme(theme));
+        restartActivity();
+    }
+
+    private int getNewTheme(int theme) {
+        return theme == R.style.PrimaryTheme
+                ? R.style.SecondaryTheme
+                : R.style.PrimaryTheme;
+    }
+
+    private void saveNewTheme(int newTheme) {
+        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putInt(getString(R.string.saved_theme), newTheme);
+        editor.commit();
+    }
+
+    private void restartActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
